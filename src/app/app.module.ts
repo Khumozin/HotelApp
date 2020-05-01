ab31bf444375fd06e19bcf039895e0f7ac24d192
Khumozin <Khumomogorosi@gmail.com>
2020-05-01 17:57:43 +0200

    Fix dashboard routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { AppComponent } from './app.component';
 import { SignInComponent } from './components/authentication/sign-in/sign-in.component';
 import { SignUpComponent } from './components/authentication/sign-up/sign-up.component';
 import { CardComponent } from './components/card/card.component';
+import { DataTableComponent } from './components/data-table/data-table.component';
 import { FeatureListComponent } from './components/feature-list/feature-list.component';
 import { GallleryCardComponent } from './components/galllery-card/galllery-card.component';
 import { LoadingComponent } from './components/loading/loading.component';
@@ -19,7 +20,11 @@ import { TableComponent } from './components/table/table.component';
 import { BookingComponent } from './pages/booking/booking.component';
 import { CartComponent } from './pages/cart/cart.component';
 import { ConfirmationComponent } from './pages/confirmation/confirmation.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { MainComponent } from './pages/dashboard/main/main.component';
+import { RoomTypesComponent } from './pages/dashboard/room-types/room-types.component';
 import { HomeComponent } from './pages/home/home.component';
+import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
 import { PaymentComponent } from './pages/payment/payment.component';
 import { RoomComponent } from './pages/room/room.component';
 import { ContactComponent } from './sections/contact/contact.component';
@@ -31,11 +36,15 @@ import { SliderComponent } from './sections/slider/slider.component';
 import { WelcomeComponent } from './sections/welcome/welcome.component';
 import { EnumToArrayPipe } from './shared/pipes/enum-to-array.pipe';
 import { AppRoutingModule } from './shared/routing/app-routing.module';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { RoomTypesComponent } from './pages/room-types/room-types.component';
-import { DataTableComponent } from './components/data-table/data-table.component';
-import { MainComponent } from './pages/main/main.component';
-import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
+import { MaintenanceComponent } from './pages/dashboard/maintenance/maintenance.component';
+import { SystemReportsComponent } from './pages/dashboard/system-reports/system-reports.component';
+import { OtherReportsComponent } from './pages/dashboard/other-reports/other-reports.component';
+import { AmenitiesComponent } from './pages/dashboard/amenities/amenities.component';
+import { RoomDetailsComponent } from './pages/dashboard/room-details/room-details.component';
+import { BookingsComponent } from './pages/dashboard/bookings/bookings.component';
+import { MessagesComponent } from './pages/dashboard/messages/messages.component';
+import { UsersComponent } from './pages/dashboard/users/users.component';
+import { SettingsComponent } from './pages/dashboard/settings/settings.component';
 
 @NgModule({
   declarations: [
@@ -70,7 +79,16 @@ import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.com
     RoomTypesComponent,
     DataTableComponent,
     MainComponent,
-    PageNotFoundComponent
+    PageNotFoundComponent,
+    MaintenanceComponent,
+    SystemReportsComponent,
+    OtherReportsComponent,
+    AmenitiesComponent,
+    RoomDetailsComponent,
+    BookingsComponent,
+    MessagesComponent,
+    UsersComponent,
+    SettingsComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/pages/dashboard/system-reports/system-reports.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-system-reports',
+  templateUrl: './system-reports.component.html',
+  styleUrls: ['./system-reports.component.scss']
+})
+export class SystemReportsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
